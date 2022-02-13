457a384f2b0b738ed00b56efc7cc55230b7747be
Omar Atef <20190356@stud.fci-cu.edu.eg>
2022-02-13 15:33:05 +0200

    UserManager Code Added (Not Complete)

--- src/main/java/Controller/UserManager.java
+++ src/main/java/Controller/UserManager.java
@@ -1,4 +1,55 @@
 package Controller;
 
+import Model.Action;
+import Model.Role;
+import Model.User;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
 public class UserManager {
+    private Hashtable<Action, ArrayList<Role>> permissions;
+    private User currentUser;
+    private static UserManager userManager;
+
+
+    private UserManager() {
+        this.permissions = new Hashtable<>();
+        initPermissions();
+    }
+
+    public static synchronized UserManager getInstance() {
+        if (userManager == null) {
+            userManager = new UserManager();
+        }
+        return userManager;
+    }
+
+    public void initPermissions() {
+        for (Action action : Action.values()) {
+            permissions.put(action, new ArrayList<Role>());
+            permissions.get(action).add(Role.ADMIN);
+        }
+        permissions.get(Action.VIEW_SHELF).add(Role.EMPLOYEE);
+        permissions.get(Action.EDIT_SHELF).add(Role.EMPLOYEE);
+
+    }
+
+//    public User login(String username, String password) {
+//        //TODO ht-login el user mn el DB
+//
+//    }
+
+    public void register(String username, String password){
+        User registered = new User(username,password);
+        //TODO ht-add el user fl DB
+    }
+
+    public static void main(String[] args) {
+
+        UserManager userManager = UserManager.getInstance();
+        System.out.println(userManager.permissions);
+
+
+    }
 }
--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,4 +1,19 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class User {
+
+    private String userName;
+    private String password;
+    private Role role;
+    private ArrayList<Report> SessionReports; //TODO daily session report wala kol el invoices!
+
+    public User (String userName,String password){
+        this.userName=userName;
+        this.password=password;
+        this.role=Role.EMPLOYEE;
+        this.SessionReports=new ArrayList<>();
+
+    }
 }
