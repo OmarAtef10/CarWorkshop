c12eba68475042bd6192110994f120bea1dcbf12
[Omar Khaled Mohy] <[omarkhaledm21@gmail.com]>
2022-03-13 18:38:07 +0200

    Testing / Updates

--- src/main/java/Service/CSV_Handler.java
+++ src/main/java/Service/CSV_Handler.java
@@ -22,10 +22,13 @@ import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.*;
 
-public class CSV_Handler implements ICSVService{
-    private final int DAILY = -1;
-    private final int MONTHLY = -1;
-    private int MODE = MONTHLY;
+enum CSV_MODES {
+    DAILY,
+    MONTHLY
+}
+
+public class CSV_Handler implements ICSVService {
+    private CSV_MODES MODE = null;
 
     public static void main(String[] args) {
         DBContext.getDBContext().setDbService(new SQLiteService());
@@ -33,12 +36,13 @@ public class CSV_Handler implements ICSVService{
         CSV_Handler cc = new CSV_Handler();
         cc.getMonthlyReportCSV();
     }
+
     @Override
     public void getMonthlyReportCSV() {
-        this.MODE = MONTHLY;
+        this.MODE = CSV_MODES.MONTHLY;
         try {
             getCSVReport();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -46,53 +50,58 @@ public class CSV_Handler implements ICSVService{
     @Override
     public void getDailyReportCSV() {
         String date = TimeUtility.getCurrentDate();
-        this.MODE = DAILY;
+        this.MODE = CSV_MODES.DAILY;
         try {
             getCSVReport(date);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     private void getCSVReport(String date) throws IOException {
         InvoiceDao invoiceDao = new InvoiceDao();
+        String reformedDate = TimeUtility.getReformedDate(date);
         ArrayList<Invoice> invoices = invoiceDao.getDailyInvoices(date);
         ArrayList<Cart> carts = getMonthlyCarts(invoices);
-        HashMap<String,Integer> products = getMonthlyProducts(carts);
+        HashMap<String, Integer> products = getMonthlyProducts(carts);
         generateCSV(products);
     }
 
     private void getCSVReport() throws IOException {
         ArrayList<Invoice> invoices = getMonthlyInvoices();
         ArrayList<Cart> carts = getMonthlyCarts(invoices);
-        HashMap<String,Integer> products = getMonthlyProducts(carts);
+        HashMap<String, Integer> products = getMonthlyProducts(carts);
         generateCSV(products);
     }
 
-    private ArrayList<Invoice> getMonthlyInvoices(){
+    private ArrayList<Invoice> getMonthlyInvoices() {
         InvoiceDao invoiceDao = new InvoiceDao();
         LocalDateTime dateTime = LocalDateTime.now();
         ArrayList<Invoice> invoices = new ArrayList<>();
 
-        int start =1;
+        int start = 1;
         int end = dateTime.getDayOfMonth();
-        String dateLooper ="";
-        String day ="";
-        String month="";
-        if(dateTime.getMonthValue()<10){
-            month="0";
-            month+=String.valueOf(dateTime.getMonthValue());
+        String dateLooper = "";
+        String day = "";
+        String month = "";
+        if (dateTime.getMonthValue() < 10) {
+            month = "0";
+            month += String.valueOf(dateTime.getMonthValue());
+        } else {
+            month = String.valueOf(dateTime.getMonthValue());
         }
 
-        while (start<= end){
-            day="";
-            if(start<10){
-                day+="0";
-                day+= String.valueOf(start);
+        while (start <= end) {
+            day = "";
+            if (start < 10) {
+                day += "0";
+                day += String.valueOf(start);
+            } else {
+                day = String.valueOf(start);
             }
-            dateLooper = dateTime.getYear()+"-"+month+"-"+day +"T";
-            if( invoiceDao.getDailyInvoices(dateLooper).size() != 0){
-                ArrayList<Invoice> temp_invoices = invoiceDao.getDailyInvoices( dateLooper);
+            dateLooper = dateTime.getYear() + "-" + month + "-" + day + "T";
+            if (invoiceDao.getDailyInvoices(dateLooper).size() != 0) {
+                ArrayList<Invoice> temp_invoices = invoiceDao.getDailyInvoices(dateLooper);
                 invoices.addAll(temp_invoices);
             }
             start++;
@@ -101,24 +110,24 @@ public class CSV_Handler implements ICSVService{
         return invoices;
     }
 
-    private ArrayList<Cart> getMonthlyCarts(ArrayList<Invoice> invoices){
+    private ArrayList<Cart> getMonthlyCarts(ArrayList<Invoice> invoices) {
         ArrayList<Cart> carts = new ArrayList<>();
-        for(Invoice invoice : invoices){
-            carts.add( invoice.getCart());
+        for (Invoice invoice : invoices) {
+            carts.add(invoice.getCart());
         }
         return carts;
     }
 
-    private HashMap<String,Integer> getMonthlyProducts(ArrayList<Cart> carts){
-        HashMap<String ,Integer> products = new HashMap<>();
-        for(Cart cart : carts){
+    private HashMap<String, Integer> getMonthlyProducts(ArrayList<Cart> carts) {
+        HashMap<String, Integer> products = new HashMap<>();
+        for (Cart cart : carts) {
             HashMap<String, Integer> cartProducts = cart.getProducts();
-            for(String prodId : cartProducts.keySet()){
-                if(products.containsKey(prodId)){
+            for (String prodId : cartProducts.keySet()) {
+                if (products.containsKey(prodId)) {
                     int current = cartProducts.get(prodId);
                     int total_current = products.get(prodId) + current;
                     products.replace(prodId, total_current);
-                }else{
+                } else {
                     products.put(prodId, cartProducts.get(prodId));
                 }
             }
@@ -126,27 +135,40 @@ public class CSV_Handler implements ICSVService{
         return products;
     }
 
-    private void generateCSV(HashMap<String,Integer> products) throws IOException{
+    private void generateCSV(HashMap<String, Integer> products) throws IOException {
+        if (products.size() == 0) {
+            return;
+        }
         ProductDao productDao = new ProductDao();
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet spreadsheet = workbook.createSheet("Report Data");
         XSSFRow row;
-
+        double grand_total = 0.0;
+        double grand_total_profit = 0.0;
         Map<String, Object[]> reportData = new TreeMap<String, Object[]>();
 
-        reportData.put("1", new Object[] { "Product ID", "Product Name", "Units Sold","Total Price","Total Profit" });
-        int counter =2;
-        for(String key : products.keySet()){
+        reportData.put("1", new Object[]{"Product ID", "Product Name", "Units Sold", "Total Price", "Total Profit"});
+        int counter = 2;
+        for (String key : products.keySet()) {
             Product p = productDao.getOrNull(key);
-            Object[] objj = new Object[5];
-            objj[0]=key;
-            objj[1]=p.getProductName();
-            objj[2]= products.get(key);
-            objj[3] =( products.get(key) * p.getPricePerUnit() );
-            objj[4]=( ( products.get(key) * p.getPricePerUnit() ) - ( p.getMarketPrice()*products.get(key) ) );
-            reportData.put( String.valueOf(counter) ,objj );
+            Object[] row_data = new Object[5];
+            row_data[0] = key;
+            row_data[1] = p.getProductName();
+            row_data[2] = products.get(key);
+            row_data[3] = (products.get(key) * p.getPricePerUnit());
+            grand_total += ((Double)row_data[3]);
+            row_data[4] = ((products.get(key) * p.getPricePerUnit()) - (p.getMarketPrice() * products.get(key)));
+            grand_total_profit += ((Double)row_data[4]);
+            reportData.put(String.valueOf(counter), row_data);
             counter++;
         }
+        Object[] row_totals = new Object[5];
+        row_totals[0]= "";
+        row_totals[1]="";
+        row_totals[2]="Grand Totals:";
+        row_totals[3]=grand_total;
+        row_totals[4]=grand_total_profit;
+        reportData.put(String.valueOf(counter),row_totals);
 
         Set<String> keyid = reportData.keySet();
 
@@ -162,11 +184,11 @@ public class CSV_Handler implements ICSVService{
 
             for (Object obj : objectArr) {
                 Cell cell = row.createCell(cellid++);
-                if(obj instanceof Integer){
-                    cell.setCellValue((Integer)obj);
-                }else if(obj instanceof Double){
+                if (obj instanceof Integer) {
+                    cell.setCellValue((Integer) obj);
+                } else if (obj instanceof Double) {
                     cell.setCellValue((Double) obj);
-                }else{
+                } else {
                     cell.setCellValue((String) obj);
                 }
             }
@@ -175,10 +197,10 @@ public class CSV_Handler implements ICSVService{
         Path currentRelativePath = Paths.get("");
         String s = currentRelativePath.toAbsolutePath().toString();
         FileOutputStream out = null;
-        if(this.MODE == MONTHLY) {
-          out = new FileOutputStream(
+        if (this.MODE == CSV_MODES.MONTHLY) {
+            out = new FileOutputStream(
                     new File(s + "/MonthlyReportM" + LocalDateTime.now().getMonthValue() + "Y" + LocalDateTime.now().getYear() + ".xlsx"));
-        }else{
+        } else {
             out = new FileOutputStream(
                     new File(s + "/DailyReportD" + LocalDateTime.now().getDayOfMonth() + "M" + LocalDateTime.now().getMonthValue() + ".xlsx"));
         }
