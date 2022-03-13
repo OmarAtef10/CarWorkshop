620a8f84f6ba5bce3dc0e7c458d8106865b4842e
Vcelist <petoilluis@gmail.com>
2022-03-13 19:52:31 +0200

    added invoice details

--- src/main/java/View/InvoiceDetailsWindow.java
+++ src/main/java/View/InvoiceDetailsWindow.java
@@ -1,5 +1,6 @@
 package View;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
@@ -30,10 +31,11 @@ public class InvoiceDetailsWindow extends VBox{
     public static final String FXML_NAME = InvoiceDetailsWindow.class.getSimpleName() + ".fxml";
     private FXMLLoader loader;
     private Stage stage;
-    private Invoice invoice;
+    private InvoiceDetail details;
 
     public InvoiceDetailsWindow() {
         loader = WindowLoader.getLoader(FXML_NAME);
+        details = new InvoiceDetail();
         try {
             loader.setRoot(this);
             loader.setController(this);
@@ -66,26 +68,19 @@ public class InvoiceDetailsWindow extends VBox{
     }
 
     private void setData(Invoice invoice){
-        CustomerDao dao = new CustomerDao();
-        ProductDao dao2 = new ProductDao();
-        Customer customer = dao.getCustomer(invoice.getCustomerId());
+        details = new InvoiceDetail(invoice);
         
-        carLbl.setText(customer.getCarModel());
-        customerNameLbl.setText(customer.getName());
-        customerPhoneLbl.setText(customer.getMobileNumber());
+        carLbl.setText(details.getCustomerCarModel());
+        customerNameLbl.setText(details.getCustomerName());
+        customerPhoneLbl.setText(details.getCustomerPhone());
 
-        dateLbl.setText(TimeUtility.getReformedDate(invoice.getDate()));
-        totalLbl.setText(String.valueOf(invoice.getTotalPaid()));
-        invoiceLbl.setText(invoice.getInvoiceId());
-        usernameLbl.setText(invoice.getUserName());
+        dateLbl.setText(details.getInvoiceDate());
+        totalLbl.setText(String.valueOf(details.getInvoiceTotal()));
+        invoiceLbl.setText(details.getInvoiceId());
+        usernameLbl.setText(details.getCashierName());
 
 
-        HashMap<String, Integer> products =  invoice.getCart().getProducts();
-        for(String id: products.keySet()){
-            Product product = dao2.getOrNull(id);
-            CartItem item = new CartItem(product.getProductName(), products.get(id), product.getPricePerUnit());
-            productsTable.getItems().add(item);
-        }
+        productsTable.getItems().addAll(details.getItems());
     }
 
     @FXML
@@ -106,7 +101,6 @@ public class InvoiceDetailsWindow extends VBox{
     @FXML
     private Label carLbl;
     
-
     @FXML
     private Label customerNameLbl;
 
@@ -145,8 +139,112 @@ public class InvoiceDetailsWindow extends VBox{
 
         Optional<Printer> chosenPrinter = printerPrompt.showAndWait();
         chosenPrinter.ifPresent(printer -> {
-            System.out.println(printer.getName());
+            //TODO: PRINT HERE
         });
     }
 
+    public InvoiceDetail getDetails() {
+        return details;
+    }
+
+    public void setDetails(InvoiceDetail details) {
+        this.details = details;
+    }
+
+}
+
+
+class InvoiceDetail {
+    String invoiceId;
+    String customerName;
+    String customerPhone;
+    String customerCarModel;
+    String cashierName;
+    String invoiceDate;
+    double invoiceTotal;
+    ArrayList<CartItem> items;
+
+    public InvoiceDetail(Invoice invoice){
+        CustomerDao dao = new CustomerDao();
+        ProductDao dao2 = new ProductDao();
+        Customer customer = dao.getCustomer(invoice.getCustomerId());
+        items = new ArrayList<>();
+        
+        setCashierName(invoice.getUserName());
+        setCustomerCarModel(customer.getCarModel());
+        setCustomerName(customer.getName());
+        setCustomerPhone(customer.getMobileNumber());
+        setInvoiceDate(invoice.getDate());
+        setInvoiceId(invoice.getInvoiceId());
+        setInvoiceTotal(invoice.getTotalPaid());
+        
+        HashMap<String, Integer> products =  invoice.getCart().getProducts();
+        for(String id: products.keySet()){
+            Product product = dao2.getOrNull(id);
+            CartItem item = new CartItem(product.getProductName(), products.get(id), product.getPricePerUnit());
+            items.add(item);
+        }
+    }
+
+    public InvoiceDetail(String invoiceId, String customerName, String customerPhone, String customerCarModel,
+            String cashierName, String invoiceDate, double invoiceTotal) {
+        this.invoiceId = invoiceId;
+        this.customerName = customerName;
+        this.customerPhone = customerPhone;
+        this.customerCarModel = customerCarModel;
+        this.cashierName = cashierName;
+        this.invoiceDate = invoiceDate;
+        this.invoiceTotal = invoiceTotal;
+    }
+
+    public InvoiceDetail(){}
+
+    public String getInvoiceId() {
+        return invoiceId;
+    }
+    public void setInvoiceId(String invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+    public String getCustomerCarModel() {
+        return customerCarModel;
+    }
+    public void setCustomerCarModel(String customerCarModel) {
+        this.customerCarModel = customerCarModel;
+    }
+    public String getCashierName() {
+        return cashierName;
+    }
+    public void setCashierName(String cashierName) {
+        this.cashierName = cashierName;
+    }
+    public String getInvoiceDate() {
+        return invoiceDate;
+    }
+    public void setInvoiceDate(String invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+    public double getInvoiceTotal() {
+        return invoiceTotal;
+    }
+    public void setInvoiceTotal(double invoiceTotal) {
+        this.invoiceTotal = invoiceTotal;
+    }
+    public ArrayList<CartItem> getItems() {
+        return items;
+    }
+    public void setItems(ArrayList<CartItem> items) {
+        this.items = items;
+    }
 }
