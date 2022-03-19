57d1a4bf2a4ee4f3d18625f8e42f22eccfa62cf8
[Omar Khaled Mohy] <[omarkhaledm21@gmail.com]>
2022-03-19 22:06:30 +0200

    Testing / Fixed Bugs

--- src/main/java/Service/PDF_Invoice.java
+++ src/main/java/Service/PDF_Invoice.java
@@ -3,16 +3,27 @@ package Service;
 import java.io.FileOutputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 import Controller.UserManager;
+import CustomizedUtilities.TimeUtility;
 import Model.InvoiceDetail;
 import View.CartItem;
+
+import Controller.UserManager;
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
+
+import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
+
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
 
@@ -32,86 +43,113 @@ public class PDF_Invoice {
 
     public String getInvoicePDF() {
         FILE = s + "\\Invoice.pdf";
-        try {
-            Document document = new Document();
-            PdfWriter.getInstance(document, new FileOutputStream(FILE));
-            document.open();
-            addMetaData(document);
-            addTitlePage(document);
-            document.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        createPDF();
         return FILE;
     }
 
-    private void addMetaData(Document document) {
-        document.addTitle("Invoice");
-        document.addSubject("Using iText");
-        document.addKeywords("Java, PDF, iText");
-        document.addAuthor("Lars Vogel");
-        document.addCreator("Lars Vogel");
-    }
+    private void createPDF() {
 
-    private void addTitlePage(Document document) throws DocumentException {
-        Paragraph preface = new Paragraph();
-        addEmptyLine(preface, 1);
-        preface.add(new Paragraph(UserManager.workshopName, catFont));
-        addEmptyLine(preface, 1);
+        Document doc = new Document();
+        PdfWriter docWriter = null;
 
-        ArrayList<CartItem> cartItems = this.invoiceDetail.getItems();
-        ArrayList<String> formattedItems = new ArrayList<>();
-        String strFormat = "%-25s %-25s %-25s %-25s";
-        String s1= "PRODUCT NAME"+insertEmptySpace(40 - "PRODUCT NAME".length());
-        String s2="PRICE"+insertEmptySpace(40 - "PRICE".length());
-        String s3="UNITS"+insertEmptySpace(40 - "UNITS".length());
-        String s4="TOTAL"+insertEmptySpace(40 - "TOTAL".length());
-        formattedItems.add(
-                (s1+s2+s3+s4)
-        );
-        if (cartItems != null) {
-            double grandTotal = 0.0;
-            for (int i = 1; i <= cartItems.size(); i++) {
-                CartItem cartItem = cartItems.get(i - 1);
-                double total = cartItem.getUnits() * cartItem.getPrice();
-                grandTotal += total;
+        DecimalFormat df = new DecimalFormat("0.00");
 
-                String ss1= cartItem.getName()+insertEmptySpace(40 - cartItem.getName().length());
-                String ss2=cartItem.getPrice()+insertEmptySpace(40 - String.valueOf(cartItem.getPrice()).length());
-                String ss3=cartItem.getUnits()+insertEmptySpace(40 - String.valueOf(cartItem.getUnits()).length());
-                String ss4=total+insertEmptySpace(40 - String.valueOf(total).length());
+        try {
 
-                formattedItems.add(
-                        (ss1+ss2+ss3+ss4)
-                );
+            //special font sizes
+            Font bfBold12 = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD, new BaseColor(0, 0, 0));
+            Font bfBold16 = new Font(Font.FontFamily.TIMES_ROMAN, 16, Font.BOLD, new BaseColor(0, 0, 0));
+            Font bf12 = new Font(Font.FontFamily.TIMES_ROMAN, 12);
+
+            //file path
+            String path = FILE;
+            docWriter = PdfWriter.getInstance(doc, new FileOutputStream(path));
+
+            //document header attributes
+            doc.addAuthor("John Cena");
+            doc.addCreationDate();
+            doc.addProducer();
+            doc.addCreator("");
+            doc.addTitle("");
+            doc.setPageSize(PageSize.A4);
+
+            doc.open();
+
+            Paragraph paragraph = new Paragraph(UserManager.workshopName, bfBold16);
+
+            //specify column widths
+            float[] columnWidths = {4.5f, 2f, 2f, 2f};
+            //create PDF table with the given widths
+            PdfPTable table = new PdfPTable(columnWidths);
+            // set table width a percentage of the page width
+            table.setWidthPercentage(90f);
+
+            //insert column headings
+            insertCell(table, "Product Name", Element.ALIGN_LEFT, 1, bfBold12);
+            insertCell(table, "Price", Element.ALIGN_LEFT, 1, bfBold12);
+            insertCell(table, "Units", Element.ALIGN_LEFT, 1, bfBold12);
+            insertCell(table, "Total", Element.ALIGN_LEFT, 1, bfBold12);
+            table.setHeaderRows(1);
+
+            //create section heading by cell merging
+            insertCell(table, "Invoice ID: " + invoiceDetail.getInvoiceId(), Element.ALIGN_LEFT, 4, bfBold12);
+            double orderTotal = 0, total = 0;
+            int items_sz = invoiceDetail.getItems().size();
+            ArrayList<CartItem> items = invoiceDetail.getItems();
+
+            for (int x = 0; x < items_sz; x++) {
+                insertCell(table, items.get(x).getName(), Element.ALIGN_LEFT, 1, bf12);
+                insertCell(table, String.valueOf(items.get(x).getPrice()), Element.ALIGN_LEFT, 1, bf12);
+                insertCell(table, String.valueOf(items.get(0).getUnits()), Element.ALIGN_LEFT, 1, bf12);
+
+                total = items.get(x).getPrice() * items.get(x).getUnits();
+                orderTotal += items.get(x).getPrice() * items.get(x).getUnits();
+                insertCell(table, df.format(total), Element.ALIGN_RIGHT, 1, bf12);
             }
-            formattedItems.add(String.format("\n%-25s", "TOTAL PAID : "));
-            formattedItems.add(String.format("%-25s", grandTotal));
-        }
-        for (int i = 0; i < formattedItems.size(); i++) {
-            preface.add(new Paragraph(formattedItems.get(i), smallNormal));
-        }
-        addEmptyLine(preface, 2);
-        //Add cashier name
-        preface.add(new Paragraph("Cashier name: " + UserManager.getInstance().getCurrentUser().getUserName() +
-                " \n" + new Date(), smallBold));
-        addEmptyLine(preface, 2);
+            //merge the cells to create a footer for that section
+            insertCell(table, "Order Total: " + df.format(orderTotal), Element.ALIGN_RIGHT, 1, bfBold12);
 
+            //add the PDF table to the paragraph
+            paragraph.add(table);
+            // add the paragraph to the document
+            doc.add(paragraph);
+            doc.add(new Paragraph(" "));
 
-        document.add(preface);
+            Paragraph p2 = new Paragraph();
+            p2.add("" + TimeUtility.getCurrentDate());
+            doc.add(p2);
+
+            Paragraph p3 = new Paragraph();
+            p3.add("Cashier Name: " + UserManager.getInstance().getCurrentUser().getUserName());
+            doc.add(p3);
+        } catch (Exception dex) {
+            dex.printStackTrace();
+        } finally {
+            //close the document
+            doc.close();
+            if (docWriter != null) {
+                //close the writer
+                docWriter.close();
+            }
+        }
     }
 
-    private static void addEmptyLine(Paragraph paragraph, int number) {
-        for (int i = 0; i < number; i++) {
-            paragraph.add(new Paragraph(" "));
+    private void insertCell(PdfPTable table, String text, int align, int colspan, Font font) {
+
+        //create a new cell with the specified Text and Font
+        PdfPCell cell = new PdfPCell(new Phrase(text.trim(), font));
+        //set the cell alignment
+        cell.setHorizontalAlignment(align);
+        //set the cell column span in case you want to merge two or more cells
+        cell.setColspan(colspan);
+        //in case there is no text and you wan to create an empty row
+        if (text.trim().equalsIgnoreCase("")) {
+            cell.setMinimumHeight(10f);
         }
+        //add the call to the table
+        table.addCell(cell);
+
     }
 
-    private static String insertEmptySpace(int n){
-        StringBuilder stringBuilder = new StringBuilder();
-        for(int i=0; i<n; i++){
-            stringBuilder.append(" ");
-        }
-        return stringBuilder.toString();
-    }
+
 }
